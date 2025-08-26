838ca6123fb54eeb32ef399346c3f53731d7d9ef
James Hu <james.hu.ustc@hotmail.com>
2025-08-27 09:18:07 +1000

    chore: add test cases

--- test/aws-utils.spec.ts
+++ test/aws-utils.spec.ts
@@ -15,9 +15,25 @@ const v3ThrottlingErrorWith400 = {
   name: 'ThrottlingException',
 };
 
+const v3ThrottlingErrorWith429 = {
+  __type: 'ThrottlingException',
+  $fault: 'client',
+  $metadata: {
+    attempts: 3,
+    httpStatusCode: 429,
+    requestId: 'bcd054d2-56c3-4238-9c28-89deabfe8c0b',
+    totalRetryDelay: 522,
+  },
+  message: 'Rate exceeded',
+  name: 'ThrottlingException',
+};
+
 describe('isPossibleAwsThrottlingError(...)', () => {
   it('works for AWS SDK v3 error with 400', () => {
     expect(isPossibleAwsThrottlingError(v3ThrottlingErrorWith400)).to.equal(true);
+  });
+  it('works for AWS SDK v3 error with 429', () => {
+    expect(isPossibleAwsThrottlingError(v3ThrottlingErrorWith429)).to.equal(true);
   });
 });
 
@@ -45,5 +61,18 @@ describe('withRetry(...)', () => {
       expect(error).to.deep.equal(v3ThrottlingErrorWith400);
     }
     expect(count).to.equal(6);
+  });
+  it('retries for AWS SDK v3 error with 429 by default', async function() {
+    this.timeout(20000);
+    let count = 0;
+    try {
+      await withRetry(async () => {
+        count ++;
+        throw v3ThrottlingErrorWith429;
+      });
+    } catch (error) {
+      expect(error).to.deep.equal(v3ThrottlingErrorWith429);
+    }
+    expect(count).to.equal(7);
   });
 });
